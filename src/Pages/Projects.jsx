79fc078e490eaccb7e1b1e8f7ea7f185adf0c9cf
Azul Cerulean <azul.sama@outlook.com>
2025-01-27 14:11:43 -0500

    added css to projects
    Signed-off-by: Azul Cerulean <azul.sama@outlook.com>

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {Link} from "react-router";
+import "./Projects.css";
 
 const Projects = () => {
     return (
-        <div>
+        <div className="projectsContainer">
             <h1>Projects</h1>
             <p>Coming soon...</p>
             <Link to={"/"}>Back to the homepage o_o</Link>
