a480bd7dde851eb4f2c1328559cb3d66167f15a6
Azul Cerulean <azul.sama@outlook.com>
2025-01-27 14:04:01 -0500

    added link to home from projects
    Signed-off-by: Azul Cerulean <azul.sama@outlook.com>

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {Link} from "react-router";
 
 const Projects = () => {
     return (
         <div>
             <h1>Projects</h1>
             <p>Coming soon...</p>
+            <Link to={"/"}>Back to the homepage o_o</Link>
         </div>
     )
 }
