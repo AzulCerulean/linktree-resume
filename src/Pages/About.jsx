b5454981abf63f94022dbdeba443895de7c830d7
Azul.Cerulean <azul.sama@outlook.com>
2025-03-18 15:21:03 -0400

    changed skills from ul to span
    Signed-off-by: Azul.Cerulean <azul.sama@outlook.com>

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,37 +1,37 @@
-import React from "react";
+//import React from "react";
 import {Link} from "react-router";
 import "./About.css";
 
 const softSkills = ["Problem Solving","Teamwork","Communication","Adaptability","Time Management","Creativity","Empathy"];
 const hardSkills = ["MongoDB","Express","React","Node.js","JavaScript","HTML","CSS","PowerShell","API","Git","GitHub","GitHubPages","Netlify",
-"GoDaddy","ServiceNow","Atlassian","Microsoft Office Suite","Google Workspace","Windows","Linux","macOS","Slack","Technical Writing",
-"WebStorm","Visual Studio","Visual Studio Code","Insomnia","Logic Pro X"];
+    "GoDaddy","ServiceNow","Atlassian","Microsoft Office Suite","Google Workspace","Windows","Linux","macOS","Slack","Technical Writing",
+    "WebStorm","Visual Studio","Visual Studio Code","Insomnia","Logic Pro X"];
 
 const About = () => {
     return (
         <div className="aboutContainer">
-            <h1>Hello there. I'm Azul :3</h1>
-            <p className="aboutMeP">MERN stack web developer with a passion for creating dynamic applications
-                . Interested in game design and music production
-                . A versatile jack-of-all-trades, always exploring new skills.</p>
+            <h1>Hello there. I&apos;m Azul :3</h1>
+            <p className="aboutMeP">If you are here, then you want to know more about me o_o
+                . Well, I&apos;ve been in the IT Support bubble for over 6 years,
+                I bring strong skills in troubleshooting, system maintenance, and user support
+                . A versatile jack-of-all-trades, always exploring new skills.
+                On my personal time, I enjoy playing games, making music, or work on a project
+                . I also got some great improvisational cooking skills, but that&apos;s reserved for my inner circles {">"}:3.
+            </p>
             <div className="skills">
                 <div className="softSkills">
                     <h2>Soft Skills</h2>
-                    {/*<ul>
-                        {softSkills.map((skill, index) => (
-                            <li key={index}>{skill}</li>
-                        ))}
-                    </ul>*/}
-                    <span>{softSkills.map(String).join(', ')}</span>
-            </div>
+                    {/*softSkills.map((skill, index) => (
+                        <p key={index}>{skill}</p>
+                    ))*/}
+                    <span className="spanSkills">{softSkills.map(String).join(", ")}</span>
+                </div>
                 <div className="hardSkills">
                     <h2>Hard Skills</h2>
-                    {/*<ul>
-                        {hardSkills.map((skill, index) => (
+                        {/*hardSkills.map((skill, index) => (
                             <li key={index}>{skill}</li>
-                        ))}
-                    </ul>*/}
-                    <span>{hardSkills.map(String).join(', ')}</span>
+                        ))*/}
+                    <span className="spanSkills">{hardSkills.map(String).join(", ")}</span>
                 </div>
             </div>
             <Link to={"/"}>Back to the homepage o_o</Link>
